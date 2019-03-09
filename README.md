6820610de0714199cc07450799afa04912460736
Alvin Ashari <alvin.ap7@gmail.com>
2019-03-09 22:32:32 +0700

    add readme.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # stockbit-test
 Backend Dev Test
+
+## IMPORT DATABASE
+import database sql (stockbit-test.sql) from folder: database, to your system database/.
+## CONFIGURATION DATABASE
+setup username, password, database credential your system at file (database.php) from folder: application/config/. 
+## SETUP SMTP EMAIL ACCOUNT
+setup your account smtp email at file (MY_Controller) from folder: application/core/.
--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -11,12 +11,9 @@ class Email extends MY_Controller {
 		$to = 'alvin.ap7@gmail.com';
 		$send = $this->send_email_contact($username, $from, $to);
 
-		// This function move to ./system/libraries/Email.php start at lines 1723
-		/* Write File
-		$text = $username.' has just send email';
-		if ($send==false)
-			$text = $username.' failed send email';
-		write_file(APPPATH.'cache/writeme.txt', $text."\n", 'a+');
+		/* Write File 
+		this function is located at /system/libraries/Email.php start at lines 1723,
+		this function called after send mail at MY_Controller line 32.
 		*/
 		
 		$this->output->set_output($send);
